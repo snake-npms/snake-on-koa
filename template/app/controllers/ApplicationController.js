0ce94a19de8fc70fb78970114237256b9d6cfe91
mars <mars@mars.com>
2018-10-19 19:06:09 +0800

    base authorize method provider

--- template/app/controllers/ApplicationController.js
+++ template/app/controllers/ApplicationController.js
@@ -1,7 +1,12 @@
 const { SnakeController } = require('snake-on-koa-core')
 class ApplicationController extends SnakeController {
-	constructor () {
-		super(...arguments)
-	}
+  constructor () {
+    super(...arguments)
+    this.use(this.authorize)
+  }
+  
+  async authorize (ctx, next) {
+    await next()
+  }
 }
 module.exports = ApplicationController
