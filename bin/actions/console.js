019aae068921c968fb27eec631cbcab9d2369437
mars <mars@mars.com>
2018-10-20 14:56:28 +0800

    add snake console

---
+++ bin/actions/console.js
@@ -0,0 +1,19 @@
+const repl = require('repl')
+const stubber = require('async-repl/stubber')
+const path = require('path')
+async function initializeContext () {
+  const Application = require(path.resolve(process.cwd(), 'config/application'))
+  let application = new Application({})
+  await application.register()
+}
+module.exports = async function () {
+  await initializeContext()
+  const replInstance = repl.start({
+    prompt: 'snake> ',
+    useGlobal: true,
+    useColors: true,
+    experimentalReplAwait: true
+    // replMode: repl.REPL_MODE_STRICT
+  })
+  stubber(replInstance)
+}
--- bin/index.js
+++ bin/index.js
@@ -4,8 +4,12 @@ const packageJson = require('../package.json');
 const { exec } = require('child_process')
 program.version(packageJson.version)
 
+// snake new project -d [mysql|sqlite3]
 program.command('new <project>')//.arguments('<project>')
 .option('-d --db <db>', 'db type', /^(sqlite3|mysql)$/i, 'sqlite3')
 .action(require('./actions/new'))
+
+// snake console
+program.command('console').action(require('./actions/console'));
 
 program.parse(process.argv);
