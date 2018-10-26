f7cd7a79be3d25c0a1e73391ad27117ee8becfe3
mars <mars@mars.com>
2018-10-26 16:25:35 +0800

    cors support cookie

--- template/app/middlewares/1.koa-cors.js
+++ template/app/middlewares/1.koa-cors.js
@@ -1,3 +1,6 @@
 // npm install --save koa2-cors
 const cors = require('koa2-cors')
-module.exports = cors()
+module.exports = cors({
+  // allow cookie, frontend eg: vue axios please set {withCredentials: true}
+  credentials: true
+})
